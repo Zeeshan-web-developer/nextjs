df4df0951cc8cbf8cafcc14249a66de6be8cfcd8
Zeeshan Ahmad <zeeshan>
2023-03-12 18:45:46 +0530

    ist api integrated

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -1,3 +1,7 @@
 export async function GET(request) {
-  return new Response('Hello, Next.js!')
+  return new Response("Hello, Next.js!")
+}
+
+export async function GET() {
+  return new Response("api first")
 }
